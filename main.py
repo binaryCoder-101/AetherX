7df8f5a8a2565f39456155d12964f327b871b938
binaryCoder-101 <syed.misak@gmail.com>
2026-08-28 23:19:59 +0530

    FastAPI entrypoint added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from aetherx!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def home():
+    return {"message": "Hello world! This is the entry point of out API!"}
