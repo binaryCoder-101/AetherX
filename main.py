c5d550986f9eba232283e432ca72b9c0bddb354a
binaryCoder-101 <syed.misak@gmail.com>
2026-08-29 18:00:55 +0530

    Added some HTML

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -19,9 +20,10 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False)
 def home():
-    return {"message": "Hello world! This is the entry point of out API!"}
+    return f"<h1>{posts[0]['title']}</h1>"
 
 @app.get("/api/posts")
 def get_posts():
