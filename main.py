77c6be3d707e2b1e623bcd4dc67f1c75c8fc8663
binaryCoder-101 <syed.misak@gmail.com>
2026-09-02 21:55:44 +0530

    feat: add Request and Response validation using Pydantic schemas and API endpoint for creating new blog/post (using POST request)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from schemas import PostCreate, PostResponse 
 
 app = FastAPI()
 
@@ -29,17 +30,39 @@ posts: list[dict] = [
 ]
 
 # API ROUTES
-@app.get("/api/posts")
+@app.get(
+        "/api/posts", 
+        response_model=list[PostResponse]
+)
 def get_posts():
     return posts
 
-@app.get("/api/posts/{post_id}")
+@app.get(
+        "/api/posts/{post_id}", 
+        response_model=PostResponse
+)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
             return post
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
+@app.post(
+        "/api/posts",
+        response_model=PostResponse,
+        status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "September 02, 2026"
+    }
+    posts.append(new_post)
+    return new_post
 
 # WEB ROUTES
 @app.get("/", include_in_schema=False, name="home")
